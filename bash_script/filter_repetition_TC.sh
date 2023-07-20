d10a8e380c2cd7d1b7fd71aeb85093de6955c921
Cheng Jed <jed351@Chengs-MBP.broadband>
2023-07-20 21:10:21 +0200

    fixed problem with TC filter

--- bash_script/filter_repetition_TC.sh
+++ bash_script/filter_repetition_TC.sh
@@ -9,16 +9,21 @@ module load anaconda3/personal
 source activate c4-env
 
 
-wget https://raw.githubusercontent.com/jedcheng/c4-dataset-script/master/c4_dataset_script/badwords/SC_list.txt
-mv SC_list.txt badwords/SC_list.txt
 
-wget https://raw.githubusercontent.com/jedcheng/c4-dataset-script/master/c4_dataset_script/Chinese/filter_out_SC_line.py
-mv filter_out_SC_line.py Chinese/filter_out_SC_line.py
 
 cd $EPHEMERAL/c4-dataset-script_$PBS_ARRAY_INDEX/c4_dataset_script
 
+rm clean_docs_repetition_TC.jsonl
+
+wget https://raw.githubusercontent.com/jedcheng/c4-dataset-script/master/c4_dataset_script/badwords/SC_list.txt
+mv SC_list.txt badwords/SC_list.txt
+
+wget https://raw.githubusercontent.com/jedcheng/c4-dataset-script/master/c4_dataset_script/Chinese/filter_out_SC_lines.py
+mv filter_out_SC_line.py Chinese/filter_out_SC_lines.py
+
+
 cat ./repetition_removal_output/clean_docs/part-* | \
-    python Chinese/filter_out_SC_lines.py \
-        --badwords_filepath ./badwords/SC_list.txt \
+    python Chinese/filter_out_SC_line.py \
+        --SC_words_filepath ./badwords/SC_list.txt \
          > clean_docs_repetition_TC.jsonl
 
